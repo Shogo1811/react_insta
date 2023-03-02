0e7d8ff8f0156e6915a095050a7624051de306b5
Shogo1811 <shogo.n.1118@gmail.com>
2023-03-02 13:33:29 +0900

    Postコンポーネント(1)

---
+++ src/features/post/Post.tsx
@@ -0,0 +1,66 @@
+import React, { useState } from "react";
+import styles from "./Post.module.css";
+
+import { makeStyles } from "@mui/material";
+import { Avatar, Divider, Checkbox } from "@mui/material";
+// import { Favorite, FavoriteBorder } from  "@mui/material";
+// import AvatarGroup from "@material-ui/lab/AvatarGroup";
+
+import { useSelector, useDispatch } from "react-redux";
+import { AppDispatch } from "../../app/store";
+
+import { selectProfiles } from "../auth/authSlice";
+
+import {
+  selectComments,
+  fetchPostStart,
+  fetchPostEnd,
+  fetchAsyncPostComment,
+  fetchAsyncPatchLiked,
+} from "./postSlice";
+
+import { PROPS_POST } from "../types";
+
+const useStyles = makeStyles((theme) => ({
+  small: {
+    width: theme.spacing(3),
+    height: theme.spacing(3),
+    marginRight: theme.spacing(1),
+  },
+}));
+
+const Post: React.FC<PROPS_POST> = ({
+  postId,
+  loginId,
+  userPost,
+  title,
+  imageUrl,
+  liked,
+}) => {
+  const classes = useStyles();
+  const dispatch: AppDispatch = useDispatch();
+  const profiles = useSelector(selectProfiles);
+  const comments = useSelector(selectComments);
+  const [text, setText] = useState("");
+
+  const commentsOnPost = comments.filter((com) => {
+    return com.post === postId;
+  });
+
+  const prof = profiles.filter((prof) => {
+    return prof.userProfile === userPost;
+  });
+
+  const postComment = async (e: React.MouseEvent<HTMLElement>) => {
+    e.preventDefault();
+    const packet = { text: text, post: postId };
+    await dispatch(fetchPostStart());
+    await dispatch(fetchAsyncPostComment(packet));
+    await dispatch(fetchPostEnd());
+    setText("");
+  };
+
+  return <div></div>;
+};
+
+export default Post;
